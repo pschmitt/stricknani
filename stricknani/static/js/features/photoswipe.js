3960c9732a3a411f7df768c65fce5531b18f61cd
Philipp Schmitt <philipp@schmitt.co>
2026-02-08 11:48:10 +0100

    fix(photoswipe): restore close button; move OCR to bottom-right

--- stricknani/static/js/features/photoswipe.js
+++ stricknani/static/js/features/photoswipe.js
@@ -234,7 +234,7 @@
         order: 6,
         isButton: true,
         html: '<span class="pswp__icn mdi mdi-text-recognition"></span>',
-        appendTo: "bar",
+        appendTo: "wrapper",
         onClick: async (event) => {
           event.preventDefault();
           event.stopPropagation();
